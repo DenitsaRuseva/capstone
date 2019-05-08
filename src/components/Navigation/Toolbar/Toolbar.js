2b3a341fea46803ce4c6325d307e3a1c2c89cbfe
DenicaRuseva <drr_87@abv.bg>
2019-05-08 09:53:54 +0300

    add cart icon to sm

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+import NavigationItem from '../NavigationItems/NavigationItem/NavigationItem';
+import WithBadge from '../../../hoc/withBadge/withBadge';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Toolbar.css';
 
 const toolbar = (props) => (
@@ -9,6 +12,11 @@ const toolbar = (props) => (
         <nav className='desktop-only'>
             <NavigationItems badgeCount={props.badgeCount}/>
         </nav>
+        <div className='navigationItems sm-only'>
+            <NavigationItem link="/cart" clicked={props.hideSideDrawer}>
+                <WithBadge  showBadge={props.badgeCount > 0} count={props.badgeCount}><FontAwesomeIcon icon='shopping-cart' aria-hidden="true"/></WithBadge>
+            </NavigationItem>
+        </div>
     </header>
 );
 
--- src/hoc/withBadge/Badge/Badge.js
+++ src/hoc/withBadge/Badge/Badge.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Badge.css';
 
 const badge = (props) => (
-    <span className='badge'>{props.count}</span>
+    <span className='badge'>{props.count < 21 ? props.count : '20+'}</span>
 );
 
 export default badge;
